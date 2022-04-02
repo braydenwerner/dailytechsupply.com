204b1741b78470787a4628a89ccb1b9b84b6b5e2
braydenwerner <braydenwerner0@gmail.com>
2022-04-02 13:53:06 -0700

    Switched to getServerSideProps

--- client/src/pages/products/3d-printers/index.tsx
+++ client/src/pages/products/3d-printers/index.tsx
@@ -1,31 +1,13 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { gql } from '@apollo/client'
 import { client } from '../../../utils/createApolloClient'
 import { GetPrinter3dInput } from '../../../generated/graphql'
-import { useRouter } from 'next/router'
 import { printer3dProperties } from '../../../constants/constants'
 import { validateQueryParams } from '../../../utils/utils'
-interface Printer3dPageProps {
-  printers?: GetPrinter3dInput[]
-}
-const Printer3dPage: NextPage<Printer3dPageProps> = (printers) => {
-  const router = useRouter()
-
-  if (!printers || Object.keys(printers).length == 0) {
-    router.push('/404')
-  }
-
-  return (
-    <>
-      <div>3d Printer page</div>
-      <div>{JSON.stringify(printers, null, 4)}</div>
-    </>
-  )
-}
 
 //  Data is not changing very much, will be best to use getStaticProps with a revalidation
 //  However, we can't use getStaticProps because we need to have access to query string in URL to filter data
-Printer3dPage.getInitialProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   //  generate query param object
   let res = await client.query({
     variables: {
@@ -64,7 +46,20 @@ Printer3dPage.getInitialProps = async (context) => {
     `,
   })
 
-  return { printers: res.data.get3dPrinters }
+  if (!res?.data?.get3dPrinters) return { notFound: true }
+
+  return { props: { printers: res.data.get3dPrinters } }
+}
+interface Printer3dPageProps {
+  printers?: GetPrinter3dInput[]
+}
+const Printer3dPage: NextPage<Printer3dPageProps> = (printers) => {
+  return (
+    <>
+      <div>3d Printer page</div>
+      <div>{JSON.stringify(printers, null, 4)}</div>
+    </>
+  )
 }
 
 export default Printer3dPage
